b3d48215d771f43aa1c6c8d92a71bc723b5c0103
ArtemHem <sidorenko.rnd@gmail.com>
2018-10-17 20:15:28 +0300

    lesson1
    lesson1

---
+++ Apple.java
@@ -0,0 +1,7 @@
+package ru.sidorenko.jc.levelI;
+
+public class Apple extends Fruit {
+    public Apple() {
+        super.setWeight(1.0f);
+    }
+}
---
+++ Fruit.java
@@ -0,0 +1,13 @@
+package ru.sidorenko.jc.levelI;
+
+public abstract class Fruit {
+    private float weight;
+
+    float getWeight() {
+        return weight;
+    }
+
+    void setWeight(float weight) {
+        this.weight = weight;
+    }
+}
