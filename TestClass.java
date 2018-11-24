ecd8dec2d38d4c12a210b73f43f579cdfda63314
ArtemHem <sidorenko.rnd@gmail.com>
2018-11-24 18:54:47 +0300

    lesson7
    lesson7

---
+++ TestClass.java
@@ -0,0 +1,11 @@
+package ru.sidorenko.jc.levelVII;
+
+
+public class TestClass {
+
+    public static void main(String[] args) {
+        final TestClassAnnotation testClassAnnotation = new TestClassAnnotation();
+        testClassAnnotation.start();
+    }
+
+}
